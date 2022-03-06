fda8ad65db732c93514c69636bff6a2f69d34b4d
lucciii33 <angelomaiele@gmail.com>
2022-03-06 23:53:01 +0000

    baseline logo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<div className="row bg-dark">
+		<div className="row bg-dark align-items-baseline">
 			<div className="col-4">
 				<Link to="/">
-					<h6 className="">Cheers & Share</h6>
+					<h6 className="m-3">Cheers & Share</h6>
 				</Link>
 			</div>
 			<div className=" d-flex col-8 justify-content-end">
