f922baecfe341fa130a626082dbeb88ab6a3a4e2
Jarraxus <buckman.joshua@gmail.com>
2022-03-08 00:49:22 +0000

    Started cards

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -21,5 +21,15 @@ export const Home = () => (
 				<img src={rigoImage} style={{ width: '600px', height: 'auto' }} />
 			</div>
 		</div>
+		<div className="recipecards">
+			<div className="card" style={{ width: "18rem" }}>
+				<img src="https://via.placeholder.com/400x200.png" className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
 	</div>
 );
